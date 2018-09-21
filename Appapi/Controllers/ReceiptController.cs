5a97323e600d9cc6ed8a6962707fb2358f0801b2
qqqer <33872358+qqqer@users.noreply.github.com>
2018-09-21 16:51:29 +0800

    修改接收接口文档

--- Appapi/Controllers/ReceiptController.cs
+++ Appapi/Controllers/ReceiptController.cs
@@ -38,9 +38,9 @@ namespace Appapi.Controllers
             return HttpContext.Current.Session.Count != 0 ? ReceiptRepository.GetPO(Condition) : throw new HttpResponseException(HttpStatusCode.Forbidden); ;
         }//根据Condition 返回匹配的采购单信息
 
-        //Post:  /api/Receipt/PrintQRCodeByPO
+        //Post:  /api/Receipt/ReceiveCommitWithNonQRCode
         [HttpPost]
-        public string FirstCommitWithNonQRCode(Receipt Para)
+        public string ReceiveCommitWithNonQRCode(Receipt Para)
         {
             return HttpContext.Current.Session.Count != 0 ? ReceiptRepository.ReceiveCommitWithNonQRCode(Para) : throw new HttpResponseException(HttpStatusCode.Forbidden); ;
         }//根据Para提供的参数，打印收货二维码
@@ -51,9 +51,9 @@ namespace Appapi.Controllers
             return HttpContext.Current.Session.Count != 0 ? ReceiptRepository.GetSupplierName(SupplierNo) : throw new HttpResponseException(HttpStatusCode.Forbidden); ;
         }//根据供应商ID，返回供应商名称
 
-        //Post:  /api/Receipt/IsOverReceived
+        //Post:  /api/Receipt/ReceiveCommitWithQRCode
         [HttpPost]
-        public string FirstCommitWithQRCode(Receipt Para)
+        public string ReceiveCommitWithQRCode(Receipt Para)
         {
             //throw new HttpResponseException(HttpStatusCode.Forbidden);
             return HttpContext.Current.Session.Count != 0 ? ReceiptRepository.ReceiveCommitWithQRCode(Para) : throw new HttpResponseException(HttpStatusCode.Forbidden);
@@ -61,30 +61,30 @@ namespace Appapi.Controllers
         #endregion
 
         #region 进料检验接口
-        //Get:  /api/Receipt/GetIQCmessage
-        public IEnumerable<Receipt> GetRemainsOfSecondUser()
+        //Get:  /api/Receipt/GetRemainsOfIQCUser
+        public IEnumerable<Receipt> GetRemainsOfIQCUser()
         {
             return HttpContext.Current.Session.Count != 0 ? ReceiptRepository.GetRemainsOfIQCUser() : throw new HttpResponseException(HttpStatusCode.Forbidden);
         }//获取IQC信息
 
-        //Post:  /api/Receipt/UpdateIQCAmount
+        //Post:  /api/Receipt/IQCCommit
         [HttpPost]
-        public string SecondCommit(Receipt para)
+        public string IQCCommit(Receipt para)
         {
             return HttpContext.Current.Session.Count != 0 ? ReceiptRepository.IQCCommit(para) : throw new HttpResponseException(HttpStatusCode.Forbidden);
         }//更新IQC数量
         #endregion
 
         #region 入库接口
-        //Get:  /api/Receipt/GetACTMessage
-        public IEnumerable<Receipt> GetRemainsOfThirdUser()
+        //Get:  /api/Receipt/GetRemainsOfAcceptUser
+        public IEnumerable<Receipt> GetRemainsOfAcceptUser()
         {
             return HttpContext.Current.Session.Count != 0 ? ReceiptRepository.GetRemainsOfAcceptUser() : throw new HttpResponseException(HttpStatusCode.Forbidden);
         }//获取入库信息
 
-        //Post:  /api/Receipt/UpdateACTInfo
+        //Post:  /api/Receipt/AcceptCommit
         [HttpPost]
-        public string ThirdCommit(Receipt para)
+        public string AcceptCommit(Receipt para)
         {
             return HttpContext.Current.Session.Count != 0 ? ReceiptRepository.AcceptCommit(para) : throw new HttpResponseException(HttpStatusCode.Forbidden);
         }//更新入库信息
