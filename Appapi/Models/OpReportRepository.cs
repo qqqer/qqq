9d144f6af68e6d2a2fad1670db1819932abb49a2
qqqer <33872358+qqqer@users.noreply.github.com>
2019-01-21 14:34:26 +0800

    ws

--- Appapi/Models/OpReportRepository.cs
+++ Appapi/Models/OpReportRepository.cs
@@ -743,7 +743,7 @@ namespace Appapi.Models
 
             if (theReport.ErpCounter < 1) //没交互过，或第一次交互失败,允许更改数量
             {
-                sql = "update bpm set " +
+                sql = " update bpm set " +
                        "DMRQualifiedQty = " + DMRInfo.DMRQualifiedQty + ", " +
                        "DMRRepairQty = " + DMRInfo.DMRRepairQty + "," +
                        "DMRUnQualifiedQty = " + DMRInfo.DMRUnQualifiedQty + "," +
