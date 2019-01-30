a5b862773e540781e4df81dcd181c8121bb0bf38
qqqer <33872358+qqqer@users.noreply.github.com>
2019-01-30 13:00:19 +0800

    ws

--- ERPAPI/OpReportRepository.cs
+++ ERPAPI/OpReportRepository.cs
@@ -89,21 +89,49 @@ namespace ErpAPI
                 LaborDataSet.LaborDtlDataTable dtLabDtl = new LaborDataSet.LaborDtlDataTable();
                 //Dim fbg, runqty, outTime As Decimal
                 decimal outTime;
-                int labHedSeq;
 
-                labHedSeq = 0;
-                if (labHedSeq == 0)
+
+                #region 下挂时间明细
+                string sql = @"select * from TACSeq where GroupID = '" + Character05 + "'";
+                DataTable dt2 = Common.SQLRepository.ExecuteQueryToDataTable(Common.SQLRepository.APP_strConn, sql);
+                int LaborHedSeq;
+
+                if (dt2 != null) 
+                {
+                    string OriDay = ((DateTime)dt2.Rows[0]["Date"]).ToString("yyyy-MM-dd");//截取从数据库获得的时间的年月日部分
+                    string today = DateTime.Now.ToString("yyyy-MM-dd");//截取当前时间的年月日部分
+
+                    if (OriDay == today) // 如果从数据库获得的日期 是今天 
+                    {
+                        LaborHedSeq = (int)dt2.Rows[0]["LaborHedSeq"];
+                    }
+                    else // 不是今天 
+                    {
+                        labAd.GetNewLaborHed1(dsLabHed, empid, false, System.DateTime.Today);
+                        dtLabHed = dsLabHed.LaborHed;
+                        labAd.Update(dsLabHed);
+                        LaborHedSeq = Convert.ToInt32(dtLabHed.Rows[0]["LaborHedSeq"]);
+
+                        sql = "UPDATE TACSeq SET LaborHedSeq = " + LaborHedSeq + ", date = getdate() where GroupID = '" + Character05 + "'";
+                    }
+                }
+                else//资源组还未添加
                 {
                     labAd.GetNewLaborHed1(dsLabHed, empid, false, System.DateTime.Today);
                     dtLabHed = dsLabHed.LaborHed;
                     labAd.Update(dsLabHed);
-                    labHedSeq = Convert.ToInt32(dtLabHed.Rows[0]["LaborHedSeq"]);
+                    LaborHedSeq = Convert.ToInt32(dtLabHed.Rows[0]["LaborHedSeq"]);
+
+                    sql = "insert into TACSeq values('" + Character05 + "', " + LaborHedSeq + ", getdate())";
                 }
-                {
-                    dsLabHed = labAd.GetByID(labHedSeq);
-                }
+                Common.SQLRepository.ExecuteNonQuery(Common.SQLRepository.APP_strConn, CommandType.Text, sql, null);
+                #endregion
+
+
+                dsLabHed = labAd.GetByID(LaborHedSeq);
+                
                 outTime  = EndDate.Hour;// + System.DateTime.Now.Minute / 100;
-                labAd.GetNewLaborDtlWithHdr(dsLabHed, System.DateTime.Today, 0, System.DateTime.Today, outTime, labHedSeq);
+                labAd.GetNewLaborDtlWithHdr(dsLabHed, System.DateTime.Today, 0, System.DateTime.Today, outTime, LaborHedSeq);
                  
                 labAd.DefaultLaborType(dsLabHed, "P");
                 labAd.DefaultJobNum(dsLabHed, JobNum);
@@ -145,7 +173,7 @@ namespace ErpAPI
 
                     if (disQty > 0)
                     {
-                        string sql = "select tranid from erp.NonConf where LaborDtlSeq = " + LaborDtlSeq + " ";
+                        sql = "select tranid from erp.NonConf where LaborDtlSeq = " + LaborDtlSeq + " ";
                         object o = Common.SQLRepository.ExecuteScalarToObject(Common.SQLRepository.ERP_strConn, CommandType.Text, sql, null);
 
                         tranid = int.Parse(o == null ? "" : o.ToString());
